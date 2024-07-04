f6183f78b3026629d10040892a5d0944b65d3c5f
LEE-NS <ogawa9341@gmail.com>
2024-07-04 22:39:15 +0900

    update : cancel import Head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "./Provider";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
